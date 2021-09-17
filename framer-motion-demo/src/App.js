19b5d8fc2d2ed95beb5f1978f5047d47fa1a3871
sambara323 <sambara323@gmail.com>
2021-09-17 13:46:41 +0000

    initail D

---
+++ framer-motion-demo/src/App.js
@@ -0,0 +1,30 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import Modal from "./components/Modal";
+
+function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const close = () => setModalOpen(false);
+  const open = () => setModalOpen(true);
+  return (
+    <div>
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="save-button"
+        onClick={() => (modalOpen ? close() : open())}
+      >
+        Launch modal
+      </motion.button>
+      <AnimatePresence
+        initial={false}
+        exitBeforeEnter={true}
+        onExitComplete={() => null}
+      >
+        {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} />}
+      </AnimatePresence>
+    </div>
+  );
+}
+
+export default App;
